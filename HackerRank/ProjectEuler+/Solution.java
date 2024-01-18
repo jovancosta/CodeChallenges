119a661619ad7ee5c8b1ab29013002ef077eb081
jovancosta <121965726+jovancosta@users.noreply.github.com>
2024-01-18 00:14:55 -0300

    Create Solution.java
    If we list all the natural numbers below  that are multiples of  or , we get  and . The sum of these multiples is .
    
    Find the sum of all the multiples of  or  below .
    
    Input Format
    
    First line contains  that denotes the number of test cases. This is followed by  lines, each containing an integer, .
    
    Constraints
    
    Output Format
    
    For each test case, print an integer that denotes the sum of all the multiples of  or  below .
    
    Sample Input 0
    
    2
    10
    100
    Sample Output 0
    
    23
    2318
    Explanation 0
    
    For , if we list all the natural numbers below  that are multiples of  or , we get  and . The sum of these multiples is .
    
    Similarly for , we get .

---
+++ HackerRank/ProjectEuler+/Solution.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            
+            long sumOfMultiples3 = calculateSumOfMultiples(n, 3);
+            long sumOfMultiples5 = calculateSumOfMultiples(n, 5);
+            long sumOfMultiples15 = calculateSumOfMultiples(n, 15);
+            
+            long sum = sumOfMultiples3 + sumOfMultiples5 - sumOfMultiples15;
+            
+            System.out.println(sum);
+        }
+    }
+    
+    // Function to calculate the sum of multiples of 'multiple' up to 'n'
+    private static long calculateSumOfMultiples(int n, int multiple) {
+        long p = (n - 1) / multiple;
+        return multiple * (p * (p + 1)) / 2;
+    }
+}
